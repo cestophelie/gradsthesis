f740e1bdde201cedcf68f8de7e5ddede59ee26ae
cestophelie <sewontheplash@gmail.com>
2021-02-02 15:43:18 +0900

    Add main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from skimage import io
 
 
 def edge_get(img):
@@ -9,7 +10,7 @@ def edge_get(img):
     scale = 1
     delta = 0
 
-    edge_result = cv2.GaussianBlur(img, (5, 5), 0)  # 가우시안 필터 적용
+    edge_result = cv2.GaussianBlur(img, (17, 17), 0)  # 가우시안 필터 적용
 
     grad_x = cv2.Sobel(edge_result, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
     grad_y = cv2.Sobel(edge_result, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
@@ -20,6 +21,22 @@ def edge_get(img):
     grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
     return grad
+
+
+def SaltPepperNoise(edgeImg):
+    count = 0
+    lastMedian = edgeImg
+    median = cv2.medianBlur(edgeImg, 3)
+    while not np.array_equal(lastMedian, median):
+        zeroed = np.invert(np.logical_and(median, edgeImg))
+        edgeImg[zeroed] = 0
+        count = count + 1
+        if count > 70:
+            break
+        lastMedian = median
+        median = cv2.medianBlur(edgeImg, 3)
+
+    return edgeImg
 
 
 def findSignificant_contour(edgeImg):
@@ -51,20 +68,25 @@ def findSignificant_contour(edgeImg):
 
 
 def final(contour, edges_):
-    mask = np.zeros_like(edges_)
-    cv2.fillPoly(mask, [contour], 255)
+    mask = np.zeros_like(edges_)  # given array 와 사이즈가 같은 배열을 모두 zero 로 초기화해서 리턴
+    cv2.fillPoly(mask, [contour], 255)  # 다각형 만들기
     # calculate sure foreground area by dilating the mask
-    mapFg = cv2.erode(mask, np.ones((5, 5), np.uint8), iterations=10)
+    kernel = np.ones((3, 3), np.uint8)  # 여백의 사이즈를 정하는 커널
+    mapFg = cv2.erode(mask, kernel, iterations=10)
     # mark inital mask as "probably background"
     # and mapFg as sure foreground
     trimap = np.copy(mask)
     trimap[mask == 0] = cv2.GC_BGD
     trimap[mask == 255] = cv2.GC_PR_BGD
     trimap[mapFg == 255] = cv2.GC_FGD
+
     # visualize trimap
     trimap_print = np.copy(trimap)
     trimap_print[trimap_print == cv2.GC_PR_BGD] = 128
     trimap_print[trimap_print == cv2.GC_FGD] = 255
+    cv2.imwrite('trimap.jpg', trimap_print)
+    # cv2.imshow('in here', trimap_print)
+    # cv2.waitKey(0)
 
     return trimap_print
 
@@ -76,6 +98,7 @@ if __name__ == '__main__':
     original = cv2.imread('pants.jpg', cv2.IMREAD_COLOR)
     original = cv2.resize(original, dsize=(500, 500), interpolation=cv2.INTER_AREA)
     edges_ = edge_get(img)
+    edges_ = SaltPepperNoise(edges_)
 
     # edges_.astype(np.uint8)
     edges_ = np.asarray(edges_, np.uint8)
@@ -86,14 +109,21 @@ if __name__ == '__main__':
 
     cv2.imshow('result', edges_)
     cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
-    # contourImg = cv2.resize(contourImg, dsize=(500, 500), interpolation=cv2.INTER_AREA)
     cv2.imshow('result 2', contourImg)
 
     output = final(edge_result, edges_)
 
     cv2.imshow('result 3', output)
+
+    mask2 = np.where((output < 200), 0, 255).astype('uint8')
+    print('min : ' + str(np.max(mask2)))
+    cv2.imshow('here', mask2)
+    cv2.imwrite('mask.jpg', mask2)
+
+    img = img * mask2
+    print(img)
+    cv2.imshow('result 3', img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
